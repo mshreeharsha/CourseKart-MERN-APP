ad3665f66679a224a7d7fdf43a7550e7c2773993
ShyamB123 <shyamer3@gmail.com>
2023-05-08 22:02:45 +0530

    added dashboard features

--- frontend/src/components/routes/Private.js
+++ frontend/src/components/routes/Private.js
@@ -11,7 +11,7 @@ export default function PrivateRoute(){
 
     useEffect(()=> {
         const authCheck = async() => {
-            const res  = await axios.get('/api/vi/auth/user-auth'); 
+            const res  = await axios.get('/api/auth/user-auth'); 
             if(res.data.ok)
             {
                 setOk(true)
