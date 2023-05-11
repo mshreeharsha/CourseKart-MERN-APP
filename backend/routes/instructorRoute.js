85ac324f097ea4b9a0270d21ac1abcf69f57307c
mshreeharsha <shreeharsha.mbhat@gmail.com>
2023-05-11 19:39:50 +0530

    Changes in Instructor CRUD and Create Course in React

--- backend/routes/instructorRoute.js
+++ backend/routes/instructorRoute.js
@@ -2,18 +2,20 @@
 const express=require('express')
 const { requireSignIn, isAdmin } = require('../middleware/requireAuth')
 const router=express.Router()
+const formidable=require('express-formidable')
 
 const {createInstructorController,
     updateInstructorController,
     getSingleInstructorController,
     deleteInstructorController,
-    getAllInstructorController}=require('../controllers/instructorController')
+    getAllInstructorController,
+    getPhotoController}=require('../controllers/instructorController')
 
 //create Instructor Route
-router.post('/create-instructor',requireSignIn,isAdmin,createInstructorController)
+router.post('/create-instructor',requireSignIn,isAdmin,formidable(),createInstructorController)
 
 //update Instructor Route
-router.patch('/update-instructor/:id',requireSignIn,isAdmin,updateInstructorController)
+router.patch('/update-instructor/:id',requireSignIn,isAdmin,formidable(),updateInstructorController)
 
 //get Single Instructor
 router.get('/single-instructor/:slug',getSingleInstructorController)
@@ -23,5 +25,8 @@ router.get('/all-instructors',getAllInstructorController)
 
 //delete Instructor
 router.delete('/delete-instructor/:id',requireSignIn,isAdmin,deleteInstructorController)
+
+//Get Photo
+router.get('/instructor-photo/:pid',getPhotoController)
 
 module.exports=router
